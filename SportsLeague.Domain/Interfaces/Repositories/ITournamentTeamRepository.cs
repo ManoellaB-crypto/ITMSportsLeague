2e7bdd2a1f1bfd2d67f7e58971b76103a6ef5166
ManoellaB-crypto <bedoyamanu6644@gmail.com>
2026-03-29 22:39:05 -0500

    Phase 3- Part 1-- New 3 entities Created: Referee, Tournament and TournamentTeam

---
+++ SportsLeague.Domain/Interfaces/Repositories/ITournamentTeamRepository.cs
@@ -0,0 +1,12 @@
+﻿using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.Domain.Interfaces.Repositories
+{
+    public interface ITournamentTeamRepository : IGenericRepository<TournamentTeam>
+    {
+        Task<TournamentTeam?> GetByTournamentAndTeamAsync(int tournamentId, int teamId); // Metodo para obtener una relacion especifica entre un torneo y un equipo
+        Task<IEnumerable<TournamentTeam>> GetByTournamentAsync(int tournamentId); // Metodo para obtener todas las relaciones de equipos en un torneo especifico
+        
+    }
+
+}

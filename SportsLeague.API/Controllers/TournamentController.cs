5d93edac8e2d42a6227469d6692635a2bc129be8
ManoellaB-crypto <bedoyamanu6644@gmail.com>
2026-04-28 21:53:29 -0500

    Phase 4 completed - New table Match created, end to end

--- SportsLeague.API/Controllers/TournamentController.cs
+++ SportsLeague.API/Controllers/TournamentController.cs
@@ -101,7 +101,7 @@ Create(TournamentRequestDTO dto)
             }
         }
         [HttpPatch("{id}/status")]
-        public async Task<ActionResult> UpdateStatus(int id, UpdateStatusDTO dto)
+        public async Task<ActionResult> UpdateStatus(int id, UpdateTournamentStatusDTO dto)
         {
             try
             {
